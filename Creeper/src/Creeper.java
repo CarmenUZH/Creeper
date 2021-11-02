ff9b453feae02257343b49b0ff66c4278ef41541
silverflamez <93607848+silverflamez@users.noreply.github.com>
2021-11-02 12:15:33 -0500

    Update Creeper.java

--- Creeper/src/Creeper.java
+++ Creeper/src/Creeper.java
@@ -1,5 +1,18 @@
 public class Creeper {
-    public String name;
+    public String name; 
     public int mood;
     public int age;
+    
+    //constructor
+    public Creeper(String startName, int startAge) {
+        name = startName;
+        age = startAge;
+    }
+    
+    public void hug(){
+        mood += 25; 
+        System.out.println(name + " hisses happily");
+    }
 }
+
+    
